e798de23986ceef960a9e10b59ec6bb1f0f35c60
Anup Pokhrel <virtualanup@gmail.com>
2016-12-02 21:05:22 -0500

    Added code to check distance error

--- functions/location_functions.py
+++ functions/location_functions.py
@@ -1,3 +1,5 @@
+from geopy.distance import vincenty
+
 top_left = (42.187280, -88.043792)
 top_right = (42.187280, -87.454202)
 bottom_left = (41.591199, -88.043792)
@@ -21,3 +23,16 @@ def get_box_index(latitude, longitude):
     x = int((longitude - top_left[1]) / long_inc)
     y = int((latitude - bottom_left[0]) / lat_inc)
     return (x, y)
+
+
+if __name__ == "__main__":
+    # We assume that fixed value increase of latitude and longitude correspond to fixed
+    # increase in distance. That is not true. But, for a small geographical error,
+    # like a city, the error is negligible
+
+    print(vincenty(bottom_right, (bottom_right[0] + 24000 * 0.00001, bottom_right[1])))
+    print(vincenty(top_left, (top_left[0] - 24000 * 0.00001, top_left[1])))
+    print()
+    print(vincenty(bottom_left, (bottom_left[0], bottom_left[1] - 2400 * 0.00001)))
+    print(vincenty(top_left, (top_left[0], top_left[1] - 2400 * 0.00001)))
+
